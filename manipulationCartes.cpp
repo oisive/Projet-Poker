4ca66df153c1f8a8a50796b3fe56c65a676bebdc
oisive <lea.ceccarelli@gmail.com>
2014-11-12 14:54:53 +0100

    Create manipulationCartes.cpp

---
+++ manipulationCartes.cpp
@@ -0,0 +1,55 @@
+manipulationCartes.cpp
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "general.h" 
+
+
+// Créer une carte selon la valeur et la couleur
+Carte creerCarte(Valeur val, Couleur coul) { 
+	Carte c;
+	c.rang = coul * NBVALEURS + val -2;
+	return c;
+}
+
+// Créer une carte selon le rang
+Carte creerCarte(unsigned int rang) {
+	Carte c;
+	c.rang = rang;
+	return c;
+}
+
+
+
+// Obtenir la valeur d'une carte à partir de son rang
+unsigned int getIntVal(const Carte* c) { 
+	return c -> rang % NBVALEURS;
+}
+
+
+// Obtenir la couleur d'une carte à partir de son rang
+unsigned int getIntCoul(const Carte* c) { 
+	return c - >rang / NBVALEURS;
+}
+
+// Renvoie la valeur (en chaine de caractère) de la carte passée en paramètre pour affichage
+const char* getValeur(const Carte* c) { 
+	static char* valeurs [] = {"2", "3", "4", "5", "6", "7", "8", "9",
+					"10" ,"valet", "dame", "roi", "as"};
+		return valeurs[getIntVal(c)];
+}
+
+// Renvoie la couleur (en chaine de caractère) de la carte passée en paramètre pour affichage
+const char* getCouleur(const Carte* c){ 
+	static char* couleurs [] = {"carreau", "trefle", "coeur", "pique"};
+	// couleurs[CARREAU] =="carreau"
+	return couleurs[getIntCoul(c)];
+}
+
+
+// Afficher la carte sous forme "valeur de couleur"
+void afficherCarte(const Carte* c) { 
+	printf("%s de %s\n", getValeur(c), getCouleur(c));
+}
+
+
