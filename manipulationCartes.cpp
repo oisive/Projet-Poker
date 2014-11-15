26759a7dbbdba039efbe9ca6f51f9ece137712fb
oisive <lea.ceccarelli@gmail.com>
2014-11-15 13:26:26 +0100

    Update manipulationCartes.cpp

--- manipulationCartes.cpp
+++ manipulationCartes.cpp
@@ -1,24 +1,15 @@
-manipulationCartes.cpp
-
 #include <stdio.h>
 #include <stdlib.h>
 #include "general.h" 
 
+// fonctions indispensables
 
 // Créer une carte selon la valeur et la couleur
 Carte creerCarte(Valeur val, Couleur coul) { 
 	Carte c;
-	c.rang = coul * NBVALEURS + val -2;
+	c.rang = coul * NBVALEURS + val;
 	return c;
 }
-
-// Créer une carte selon le rang
-Carte creerCarte(unsigned int rang) {
-	Carte c;
-	c.rang = rang;
-	return c;
-}
-
 
 
 // Obtenir la valeur d'une carte à partir de son rang
@@ -26,10 +17,14 @@ unsigned int getIntVal(const Carte* c) {
 	return c -> rang % NBVALEURS;
 }
 
+unsigned int getIntVal(Carte c) { 
+	return c.rang % NBVALEURS + 2;
+}
+
 
 // Obtenir la couleur d'une carte à partir de son rang
 unsigned int getIntCoul(const Carte* c) { 
-	return c - >rang / NBVALEURS;
+	return c -> rang / NBVALEURS;
 }
 
 // Renvoie la valeur (en chaine de caractère) de la carte passée en paramètre pour affichage
@@ -47,9 +42,18 @@ const char* getCouleur(const Carte* c){
 }
 
 
+// fonctions de confort
+
+
+// Créer une carte selon le rang
+Carte creerCarte(unsigned int rang) {
+	Carte c;
+	c.rang = rang;
+	return c;
+}
+
+
 // Afficher la carte sous forme "valeur de couleur"
 void afficherCarte(const Carte* c) { 
 	printf("%s de %s\n", getValeur(c), getCouleur(c));
 }
-
-
