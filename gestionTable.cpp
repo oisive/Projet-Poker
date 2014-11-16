7d54de1a35c2a1e3a0f51c273fb8220d8913d887
oisive <lea.ceccarelli@gmail.com>
2014-11-16 17:42:53 +0100

    Update gestionTable.cpp

--- gestionTable.cpp
+++ gestionTable.cpp
@@ -6,13 +6,19 @@
 // Initialise la table passée en paramètre
 void initialiserTable(Table *desk){
 	int i; 
-	 desk->mise[MISEUR] = MISEDEPART;
-	 desk->pot = desk->mise[MISEUR];
+	desk->mise[MISEUR] = MISEDEPART;
+	for (i = 1; i < NBJOUEURS; i++){
+		if (i != MISEUR)	// securité
+		desk->mise[i] = 0;
+	}
+	desk->pot = 0;
 	for(i = 0 ; i < NBJOUEURS ; i++){
 		desk->numero[i].main = creerMain();
 		desk->numero[i].taille = TAILLECOMBINAISON;
 		desk->numero[i].mainAnalysee[desk->numero[i].taille];
 		desk->numero[i].argent = ARGENT;
+		if (desk->numero[i].position == MISEUR)
+			desk->numero[i].argent -= MISEDEPART;
 		desk->numero[i].position = i;
 	}
 }
@@ -22,7 +28,7 @@ Table creerTable(){
 	int i; 
 	Table desk;
 	desk.mise[MISEUR] = MISEDEPART;
-	desk.pot = desk.mise[MISEUR];
+	desk.pot = calculPot(&desk);
 	for(i = 0 ; i < NBJOUEURS ; i++){
 		desk.numero[i].main = creerMain();
 		desk.numero[i].taille = TAILLECOMBINAISON;
@@ -40,6 +46,7 @@ void afficherTable(Table *desk){
 	printf("Le pot est de %d\n", desk->pot);
 	for(i = 0 ; i < NBJOUEURS ; i++){
 		printf("Le joueur %d : \n", i);
+		printf("Le joueur a mise %d jetons\n", desk->mise[desk->numero[i].position]);
 		afficherMain(&(desk->numero[i].main));
 		printf("Le joueur possede %d jetons.\n",  desk->numero[i].argent); 
 		printf("La position du joueur est : %d.\n", desk->numero[i].position); 
@@ -94,6 +101,7 @@ void changementPosition(Table *desk){
 }
 
 // Ote de la reserve l'argent que les joueurs ont misé
+// devrait devenir useless 
 void updateArgent(Table *desk){
 	int i, j;
 	for (i = 0; i < NBJOUEURS; i++){ // Parcourt les joueurs
@@ -106,14 +114,153 @@ void updateArgent(Table *desk){
 	}
 }
 
+int calculPot(Table *desk){
+	unsigned int pot = 0;
+	int i;
+
+	for (i = 0; i < NBJOUEURS; i++){
+		pot += desk->mise[i];
+	}
+	return pot;
+}
+
 void updatePot(Table *desk){
 	int i;
 	for (i = 0; i < NBJOUEURS; i++){
 		desk->pot += desk->mise[i];
 	}
-
 }
 
+// Vide le pot en fin de tour
 void resetPot(Table *desk){
 	desk->pot = 0;
+}
+
+// Remet les mises à leurs valeurs initiales
+void resetMise(Table *desk){
+	int i;
+
+	desk->mise[MISEUR] = MISEDEPART;
+	for (i = 1; i < NBJOUEURS; i++){
+		desk->mise[i] = 0;
+	}
+}
+
+void resetTour(Table *desk){
+	resetMise(desk);
+	resetPot(desk);
+	changementPosition(desk);
+}
+
+// Regarde quel joueur doit mettre la grosse blind et lui prend la valeur de la grosse blind
+void blindEntree(Table *desk){
+	int i;
+
+	for (i = 0; i < NBJOUEURS; i++){
+		if (desk->numero[i].position == MISEUR){
+			if (desk->numero[i].argent >= desk->mise[MISEUR]){
+				desk->numero[i].argent -= desk->mise[MISEUR];
+				desk->pot += desk->mise[MISEUR];
+			}
+			else {
+				desk->mise[MISEUR] = desk->numero[i].argent;
+				desk->numero[i].argent = 0;
+				desk->pot += desk->mise[MISEUR];
+			}
+		}
+	}
+}
+
+// Renvoie la valeur de la mise minimale à mettre pour jouer
+int miseASuivre(Table *desk){
+	int miseASuivre = desk->mise[MISEUR];
+	int i;
+
+	for (i = 0; i < NBJOUEURS; i++){
+		if (desk->mise[i] > miseASuivre)
+			miseASuivre = desk->mise[i];
+	}
+	return miseASuivre;
+}
+
+// Dit si le joueur a assez d'argent pour suivre la mise minimale ou non
+bool peutSuivre(Table *desk, int numJoueur){
+	bool booleen = true;
+
+	if (miseASuivre(desk) > desk->numero[numJoueur].argent)
+		booleen = false;
+
+	return booleen;
+}
+
+
+void faitTapis(Table *desk, int numJoueur, int decision){ // Lui demander s'il fait tapis, stocker dans décision, 0 signifie non, le reste oui
+	int i;
+	int grosseMise = miseASuivre(desk);
+
+	if (!peutSuivre(desk, numJoueur) && decision){ // S'il ne peut pas suivre mais fait tapis
+		desk->mise[desk->numero[numJoueur].position] = desk->numero[numJoueur].argent; // Il met tout en jeu
+		for (i = 0; i < NBJOUEURS; i++){
+			if (desk->mise[desk->numero[i].position] == grosseMise){
+				desk->numero[i].argent += (desk->mise[desk->numero[i].position] - desk->mise[desk->numero[numJoueur].position]);
+				desk->mise[desk->numero[i].position] = desk->mise[desk->numero[numJoueur].position];	// Et les autres equilibrent
+			}
+		}
+	}
+	// else : rien du tout, le joueur ne continue pas le tour
+}
+
+// Renvoie true si quelqu'un n'a plus d'argent
+bool personneAuTapis(Table *desk){
+	bool booleen = true;
+	int i;
+	for (i = 0; i < NBJOUEURS; i++){
+		if (desk->numero[i].argent == 0)
+			booleen = false;
+	}
+	return booleen;
+}
+
+// Gere la mise, a perfectionner surtout sur le retour utilisateur
+void miser(Table *desk, int numJoueur, int choix){
+	int grosseMise = miseASuivre(desk);
+
+	if (choix == 0 && desk->mise[desk->numero[numJoueur].position] >= grosseMise)
+		joueurCheck();
+	else if (choix + desk->mise[desk->numero[numJoueur].position] >= miseASuivre(desk) && personneAuTapis(desk)){
+		desk->numero[numJoueur].argent -= choix;
+		desk->mise[desk->numero[numJoueur].position] += choix;
+		desk->pot += choix;
+		joueurMise(choix);
+	}
+	else if (choix < 0)
+		joueurCouche();
+	else
+		choixInvalide();
+}
+
+
+void joueurCheck(){
+	puts("Le joueur check.\n");
+}
+
+void joueurMise(int mise){
+	printf("Le joueur mise %d.\n", mise);
+}
+
+void joueurCouche(){
+	puts("Le joueur jette sa main.\n");
+}
+
+void choixInvalide(){
+	puts("Le choix n'est pas valide, veuillez recommencer.\n");
+}
+
+// Aide pour tester ses fonctions
+void afficherMises(Table *desk){
+	int i;
+	for (i = 0; i < NBJOUEURS; i++){
+		printf("%d ", desk->mise[i]);
+	}
+	puts("\n");
 }
