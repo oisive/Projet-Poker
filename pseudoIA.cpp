10fdcf94730ed42bfc70bde5242b30ef0b67873f
oisive <lea.ceccarelli@gmail.com>
2014-11-18 19:12:42 +0100

    Update pseudoIA.cpp

--- pseudoIA.cpp
+++ pseudoIA.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "general.h"
 
 
@@ -9,27 +10,50 @@ int main(){
 int probaCombinaison[NBCOMBINAISONS] = {55, 70, 85, 90, 95, 96, 97, 99, 100}; 
 int compositionMain[TAILLECOMBINAISON] = {0, 5, 0, 11, 0, 12, 0, 2, 0, 4, 0};
 Table desk; 
+
+srand(time(NULL));
+
 initialiserTable(&desk);
-recapJoueur(&desk);
+blindEntree(&desk);
+
+desk.numero[0].probabilite = 50;
+desk.mise[1] = 0;
+afficherMises(&desk);
 puts("\n");
+choixParole(&desk, 0);
+puts("\n");
+afficherTable(&desk);
 
-desk.mise[0] = 100;
-desk.mise[1] = 200;
-updatePot(&desk);
-updateArgent(&desk);
-
-recapJoueur(&desk);
-changementPosition(&desk);
-resetPot(&desk);
-puts("\n"); puts("\n");
-
-desk.mise[0] = 100;
-desk.mise[1] = 50;
-updatePot(&desk);
-updateArgent(&desk);
-recapJoueur(&desk);
-changementPosition(&desk);
 
 system("pause"); 
 return 0;
 }
+
+void choixParole(Table *desk, int numJoueur){ 
+	int nombre = 0;
+	int miseAsuivre = miseASuivre(desk);
+	int mise = 0;
+
+	nombre = (rand() % 100) + 1; 
+	
+	if(nombre < (desk->numero[numJoueur].probabilite) / 4)		// Grosse relance 3 x la mise à suivre
+		mise = (3 * miseAsuivre);
+
+
+	else if(nombre < (desk->numero[numJoueur].probabilite) / 3)			//  Relance 2x la mise à suivre
+		mise = 2 * miseAsuivre;
+
+	else if(nombre <= desk->numero[numJoueur].probabilite){
+		if(desk->mise[desk->numero[numJoueur].position] < miseAsuivre)		// S'il n'a pas misé le minimum pour suivre il le fait
+			mise = miseAsuivre - desk->mise[desk->numero[numJoueur].position]; 
+		else															  // Sinon il checke 
+			mise = 0;
+	}
+	
+	else // Il se couche
+		mise = -1;
+	printf("%d\n", nombre);
+	miser(desk, numJoueur, mise);				// La fonction miser fait en sorte que s'il n'a pas assez, il misera ce qui lui reste
+}
+
+
